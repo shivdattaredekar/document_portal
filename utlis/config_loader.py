abc7276163f8a3205a300465108cdc6a4886bead
shivdatta <shivdattaredekar@gmail.com>
2025-09-06 15:57:43 +0530

    Config code added

---
+++ utlis/config_loader.py
@@ -0,0 +1,16 @@
+import yaml
+
+
+def load_config(file_path: str) -> dict:
+    """
+    Load configuration from a Python file.
+
+    Args:
+        file_path (str): Path to the configuration file.
+
+    Returns:
+        dict: Configuration parameters as a dictionary.
+    """
+    with open(file_path, 'r') as f:
+        config = yaml.safe_load(f)
+    return config 
